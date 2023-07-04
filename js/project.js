c6288136499a4d6ebe5222c7ddf9fef1cbff33e9
leewebstechnologies <aliuajibade317@gmail.com>
2023-07-04 18:22:42 +0100

    first commit

---
+++ js/project.js
@@ -0,0 +1,66 @@
+let projectData = [
+    {
+        image: "images/project-1.png",
+        name: "project one",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-2.png",
+        name: "project two",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-3.png",
+        name: "project three",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-4.png",
+        name: "project four",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-5.png",
+        name: "project five",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-6.png",
+        name: "project six",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-7.png",
+        name: "project seven",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    },
+    {
+        image: "images/project-8.png",
+        name: "project eight",
+        detail: "Lorem ipsum dolor sit amet",
+        github: "#",
+        live: "#",
+        tags: "#javascript, #fullstack, #css"
+    }
+]
